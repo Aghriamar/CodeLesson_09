efa6228c0c03961db5407a2069ba0f7e77d21058
Aghriamar <A1exXx88@inbox.ru>
2024-05-06 09:05:21 +0300

    Adding structure Student

--- CodeLesson_09/CodeLesson_09.cpp
+++ CodeLesson_09/CodeLesson_09.cpp
@@ -1,43 +1,14 @@
 ﻿#include <iostream>
+#include <string>
 
-enum class Result
+struct Student
 {
-    SUCCESS,
-    FAIL,
-    ERROR_WRITE,
-    ERROR_READ,
-};
-
-enum class Another
-{
-    WORK,
-    SUCCESS
-};
-
-Result DoWork()
-{
-    return Result::ERROR_WRITE;
-}
-
-enum Alphabet
-{
-    A,
-    B,
-    C
+	int Age;
+	int Height;
+	std::string Name;
 };
 
 int main()
 {
-    Result workResult = DoWork();
-    std::cout << static_cast<int>(workResult);
-    static_cast<Result>(3);
-    if (workResult == Result::SUCCESS)
-    {
-
-    }
-    Another anotherRes = Another::WORK;
-    /*if (anotherRes == workResult)
-    {
-
-    }*/
+	Student* ptr = new Student;
 }
